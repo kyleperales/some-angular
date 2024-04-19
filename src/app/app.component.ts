84f47f98f089ec36321401521ea2b85948630375
Kyle <kyleperalestudio@gmail.com>
2024-04-19 16:15:28 +0800

    Integrate StatusListingComponent to AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { StatusListingComponent } from './status-listing/status-listing.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, StatusListingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
